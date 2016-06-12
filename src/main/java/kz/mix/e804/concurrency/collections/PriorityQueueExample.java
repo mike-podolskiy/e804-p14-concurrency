a8df6b5c6cdc197cb537b81f8339cac775f6f549
Михаил Подольский <mpodolskii@inbox.ru>
2016-06-12 18:44:06 +0600

    Concurrent collections

---
+++ src/main/java/kz/mix/e804/concurrency/collections/PriorityQueueExample.java
@@ -0,0 +1,28 @@
+//CHECKSTYLE:OFF
+package kz.mix.e804.concurrency.collections;
+
+import java.util.*;
+
+// Simple PriorityQueue example. Here, we create two threads in which one thread inserts an element,
+// and another thread removes an element from the priority queue.
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        final PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        // spawn a thread that removes an element fom the priority queue
+        new Thread() {
+            public void run() {
+                // Use remove() method in PriorityQueue to remove the element if available
+                System.out.println("The removed element is: " + priorityQueue.remove());
+            }
+        }.start();
+
+        // spawn a thread that inserts an element into the priority queue
+        new Thread() {
+            public void run() {
+                // insert Integer value 10 as an entry into the priority queue
+                priorityQueue.add(10);
+                System.out.println("Successfully added an element to the queue ");
+            }
+        }.start();
+    }
+}
